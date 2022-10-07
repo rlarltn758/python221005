b34ddd2e8f6da0b4e4f56793a7eaa5d228a77c40
kisukim <rlarltn759@naver.com>
2022-10-07 15:46:42 +0900

    크롤링+pandas

---
+++ 오늘의유머.py
@@ -0,0 +1,36 @@
+# coding:utf-8
+from bs4 import BeautifulSoup
+import urllib.request
+import re 
+
+#User-Agent를 조작하는 경우(아이폰에서 사용하는 사파리 브라우져의 헤더) 
+hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
+
+for n in range(0,10):
+        #클리앙의 중고장터 주소 
+        data ='http://www.todayhumor.co.kr/board/list.php?table=humorbest&page=' + str(n)
+        #웹브라우져 헤더 추가 
+        req = urllib.request.Request(data, \
+                                    headers = hdr)
+        data = urllib.request.urlopen(req).read()
+        # 한글이 깨진는 경우 디코딩
+        page = data.decode('utf-8', 'ignore')
+        soup = BeautifulSoup(page, 'html.parser')
+        # attrs는 속성들
+        list = soup.find_all('td', attrs={'class':'subject'})
+
+        # <span class="subject_fixed" data-role="list-title-text" title="아이폰14프로맥스 256 실버 미개봉">
+        #         아이폰14프로맥스 256 실버 미개봉
+        # </span>
+        # <a href="/board/view.php?table=humorbest&amp;no=1709864&amp;s_no=1709864&amp;page=2" target="_top">오늘 베스트글 근황 (2022-10-06 목요일)</a>
+
+        for item in list:
+                try:
+                        title = item.find("a").text.strip()
+                        # print(title)
+                        if (re.search('미국', title)):
+                                print(title.strip())
+                        #         print('https://www.clien.net'  + item['href'])
+                except:
+                        pass
+        
